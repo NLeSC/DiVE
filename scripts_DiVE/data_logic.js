32f076b73dcb7855887b32c38174089e0193af1d
Sonja Georgievska <s.georgievska@esciencecenter.nl>
2017-06-14 13:57:06 +0200

    Revert "added file upload progress bar"
    This reverts commit df83b40476a1408db268b5d9f08b5f9003443aff.

--- scripts_DiVE/data_logic.js
+++ scripts_DiVE/data_logic.js
@@ -14,85 +14,12 @@ function InitGlobalDataVariables() {
             entriesColor = undefined;            
         }
 
-function abortRead() {
-    reader.abort();
-}
-
-
-function errorHandler(evt) {
-    switch (evt.target.error.code) {
-        case evt.target.error.NOT_FOUND_ERR:
-            alert('File Not Found!');
-            break;
-        case evt.target.error.NOT_READABLE_ERR:
-            alert('File is not readable');
-            break;
-        case evt.target.error.ABORT_ERR:
-            break; // noop
-        default:
-            alert('An error occurred reading this file.');
-    };
-}
-
-function updateProgress(evt) {
-    // evt is an ProgressEvent.
-    if (evt.lengthComputable) {
-        var percentLoaded = Math.round((evt.loaded / evt.total) * 100);
-        // Increase the progress bar length.
-        if (percentLoaded < 100) {
-            progress.style.width = percentLoaded + '%';
-            progress.textContent = percentLoaded + '%';
-        }
-    }
-}
-
-//https://www.html5rocks.com/en/tutorials/file/dndfiles/ 
-function LoadDataFromFile(file) {
-
-    progress.style.width = '0%';
-    progress.textContent = '0%';
-    var reader = new FileReader();
-    reader.onerror = errorHandler;
-    reader.onprogress = updateProgress;
-    reader.onabort = function (e) {
-        alert('File read cancelled');
-    };
-    reader.onloadstart = function (e) {
-        document.getElementById('progress_bar').className = 'loading';
-    };
-    reader.onload = function (e) {
-        // Ensure that the progress bar displays 100% at the end.
-        progress.style.width = '100%';
-        progress.textContent = '100%';
-        setTimeout("document.getElementById('progress_bar').className='';", 2000);
-    }
-    reader.readAsText(file);
-    dataFileName = file.name;
-    dataset = dataFileName.split("_");
-    dataset = dataset[0];
-    reader.onloadend = function (e) {
-        RemoveColorMap();
-        var contents = e.target.result;
-        error = event.target.error;
-
-        if (error != null) {
-            console.error("File could not be read! Code " + error.code);
-        } else {
-            InitGlobalDataVariables();
-            var data = JSON.parse(contents);
-            defineCombo(data);
-            InitDrawing(data);
-            loadFirstTime = false;
-        }
-    }
-
-}
-
+        
 
  /** Loads data from file and makes initializations necessary to render data points
       @param {file} file - The file selected by the user
  */
- function LoadDataFromFileOld(file) {
+ function LoadDataFromFile(file) {
         var reader = new FileReader();
         reader.readAsText(file);
          dataFileName = file.name;
